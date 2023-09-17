5e1da7ef727c0d5fb61d8cd194e2f781a3218e46
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-09-17 12:05:59 -0300

    ej. 17

--- clases/guia-2.R
+++ clases/guia-2.R
@@ -1,4 +1,4 @@
-n <- 10000
+n <- 0
 
 desvio <- abs(rnorm(1, 0, 1))
 x <- rnorm(n, mean = 0, sd = desvio)
@@ -15,6 +15,24 @@ esd <- 0
 for (i in 1:10000) {
     x <- rFinv(function(x) return((log(1-x)/-t)^(1/3)), n) #inversa generalizada de U
     tmv <- n/sum(x^3)
-    esd <- (tmv - t)^2
+    esd <- esd + (tmv - t)^2
 }
 print(esd/n)
+
+# ej 17
+n <- 3000
+t <- 1
+sd1 <- 0
+sd2 <- 0
+for (i in 1:n) {
+    x <- rFinv(function(x) return((log(1-x)/-t)), n)
+    t1 <- exp(-1/mean(x))
+    t2 <- sum(x >= 1)/n
+    sd1 <- sd1 + (t1 - exp(-t))^2
+    sd2 <- sd2 + (t2 - exp(-t))^2
+    print(c(t1, t2))
+}
+print(c("P(x>1) = ", exp(-t)))
+print(c("t1", "t2"))
+print(c(sd1/n, sd2/n))
+print(c(exp(-2*t)/t^4, exp(-t)-exp(-2*t)))
