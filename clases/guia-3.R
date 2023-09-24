6914bae6e3155105abc42119614a485c7c250fa2
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-09-24 18:07:00 -0300

    ej. 6

--- clases/guia-3.R
+++ clases/guia-3.R
@@ -6,7 +6,6 @@ icNorm = function(x, nivel, de = NA) {
     } else {
         quant = qnorm(1 - a/2)
     }
-    u = mean(x)
     return(c(u - quant*de/sqrt(length(x)), u + quant*de/sqrt(length(x))))
 }
 x = c(5.1, 5.2, 5.6, 5.1, 5.5, 5.8, 5.9, 4.9, 5.2, 5.6)
@@ -22,3 +21,9 @@ icExp = function(x, nivel) {
 x = c(25, 45 ,50, 61, 39, 40, 45 ,47, 38, 39, 54, 60, 39, 46, 39, 50, 42, 50, 62, 50)
 print(icExp(x, 0.99))
 print(length(x)/sum(x)) # Comparo el i.c. de nivel 0.99 con el MLE de muestra exponencial
+
+#ej. 6
+p_h = 0.3
+desv = p_h*(1-p_h)
+cuant = qnorm(0.05/2)
+print(c(p_h - sqrt(desv/100), p_h + sqrt(desv/100)))
